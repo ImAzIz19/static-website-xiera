162b5781509a71251b2a54563499f4cc3b283990
ImAzIz19 <habibiaziz916@gmail.com>
2023-07-16 21:20:10 +0200

    please work

--- code/code.js
+++ code/code.js
@@ -1,8 +1,24 @@
-function myFunction() {
+/* function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
-  }
+  } */
+  window.onload = function () {
+    window.addEventListener('scroll', function (e) {
+      if (window.pageYOffset > 100) {
+        document.querySelector("header").classList.add('is-scrolling');
+      } else {
+        document.querySelector("header").classList.remove('is-scrolling');
+      }
+    });
+  
+    const menu_btn = document.querySelector('.iconn22');
+    const mobile_menu = document.querySelector('.mobielnav');
+  
+    menu_btn.addEventListener('click', function () {
+      menu_btn.classList.toggle('is-active');
+      mobile_menu.classList.toggle('is-active');
+    })}
